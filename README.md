26804fbbc7cc2e31f7c09266cdf0f9e84bc351d5
angelkawaii <amechan@raincandy.u>
2023-11-22 20:14:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+# Xiaomi 13/14 Camera EXIF Fix / 小米13/14 相机 EXIF 修复
+
+一个用于 “修复” 小米13/14 相机专业模式下 DNG 文件缺少 exif 信息的脚本。    
+A script to “fix” missing exif information in DNG files in Xiaomi 13/14 series camera pro mode.
+
+在 小米13/14 上使用 专业模式 进行 Raw / UltraRaw 摄影时，dng 文件会缺少 GPS位置、镜头、焦距等 EXIF 信息，这会给使用
+Lightroom 等软件输出和管理带来很多不便。  
+此问题已向小米官方反馈，但官方员工表示此问题不会得到修复。  
+因此，该脚本从 DNG 照片伴生的 jpg 文件中复制 EXIF 信息，使得 DNG 能够“保留”原本应该存在的拍摄信息。
+
+When using Raw/UltraRaw camera mode in Pro-mode on the Xiaomi 13/14 series, the dng file will lack EXIF information
+such as GPS location, lens, focal length, etc., which will cause a lot of inconvenience for management.   
+This issue has been reported to Xiaomi official, but official said this issue will not be fixed.
+Therefore, the script copies the EXIF information from the jpg file that accompanies the DNG photo,
+allowing the DNG to "preserve" the shooting information that should otherwise be there.
+
+
+----
+
+## 支持机型 / Supported Devices
+
+- [x] Xiaomi 13 / Pro / Ultra
+- [ ] Xiaomi 14 (TBC)
+- [ ] Xiaomi 14 Pro (TBC)
+
+----
+
+## 使用方法 / Usage
+
+### 前置条件 / Precondition
+
+- Python 3.12 (已测试 / Tested)
+
+1. 推荐备份原始DNG文件 / Backup your .dng files is strongly recommended.
+2. 在运行之前，您可以编辑 ``main.py`` 中的设置项，根据需求开启或关闭。/ Before running this script, you can edit the settings in ``main.py`` and change the settings according to your needs.
+3. 打开终端，运行 / Open the terminal, run :
+
+#### 创建venv 
+> python -m venv ./venv/
+> cd venv
+> ./activate
+#### 安装依赖
+> pip install -r requirements.txt
+#### 运行
+> python main.py <dng_dir_path> [jpg_dir_path]
+
+### 开源许可证 / License
+
+MIT LICENSE
---
+++ main.py
@@ -0,0 +1,158 @@
+import logging
+import os
+import sys
+import exiftool
+from exiftool.exceptions import ExifToolExecuteError
+
+# =============Config=============
+DRY_RUN = False
+# 重写对应的机型代号到机型的名称
+ENABLE_MODEL_REWRITE = True
+# 由于raw模式下的jpg存在问题，设为True后将在处理完成后自动删除所有raw的伴生jpg
+# 注：ultra-raw 不受影响
+DELETE_TRASH_JPG = False
+
+MODEL_CONFIG = {
+    "2304FPN6DC": {
+        "NAME": "Xiaomi 13 Ultra",
+        "LensMap": {
+            2.83: "Xiaomi 13 Ultra Front UltraWide Camera",
+            2.0: "Xiaomi 13 Ultra Rear UltraWide Camera",  # Sony IMX 858
+            8.7: "Xiaomi 13 Ultra Rear Wide Camera",  # Sony IMX 989
+            9.9: "Xiaomi 13 Ultra Rear Telephoto Camera",  # Sony IMX 858
+            19.4: "Xiaomi 13 Ultra Rear Super Telephoto Camera",  # Sony IMX 858
+        }
+    },
+    "23127PN0CC": {
+        "NAME": "Xiaomi 14",
+        "LensMap": {
+        }
+    },
+    "23116PN5BC": {
+        "NAME": "Xiaomi 14 Pro",
+        "LensMap": {
+        }
+    },
+}
+
+# =============Config END=============
+
+DNG_DIR_PATH = ""
+JPG_DIR_PATH = ""
+
+basic_tags = ["EXIF:*", "EXIF:GPS*", "XMP:LensModel", "Composite:GPSPosition"]
+
+logging.basicConfig(level=logging.INFO)
+
+
+def process():
+    # 遍历raw_path中所有.dng后缀的文件，存放到列表dng_list中
+    dng_list = []
+
+    for file in os.listdir(DNG_DIR_PATH):
+        if file.endswith(".dng"):
+            dng_list.append(file)
+
+    # 使用exiftool的上下文管理器
+    with exiftool.ExifToolHelper(logger=logging) as et:
+        # 遍历dng_list中每个文件
+        for i, dng_filename in enumerate(dng_list):
+            logging.info(f"=============={dng_filename}({i + 1}/{len(dng_list)})==============")
+            # 获取.dng文件的前缀
+            prefix = dng_filename.split(".")[0]
+            # 拼接对应的.jpg文件的路径
+            jpg_full_path = os.path.join(JPG_DIR_PATH, prefix + ".jpg")
+            dng_full_path = os.path.join(DNG_DIR_PATH, dng_filename)
+
+            if not os.path.exists(jpg_full_path):
+                logging.warning(f"{jpg_full_path} does not exist! skipped.")
+                continue
+
+            jpg_exif_info = et.get_tags(jpg_full_path, basic_tags)[0]
+            logging.debug(jpg_exif_info)
+
+            exif_model = jpg_exif_info["EXIF:Model"]
+            exif_focal = jpg_exif_info["EXIF:FocalLength"]
+            rewrite_model = exif_model
+            try:
+                model_config = MODEL_CONFIG[exif_model]
+                try:
+                    lens_model = model_config["LensMap"][exif_focal]
+                    if ENABLE_MODEL_REWRITE:
+                        rewrite_model = model_config["NAME"]
+                        logging.debug(f"Model will be rewrote to {rewrite_model}")
+                    try:
+                        # 将元数据写入图片B，同时保持FileCreateDate和FileModifyDate不变，设置LensModel和Model
+                        result_tag_map = {"EXIF:FocalLength": f"'{exif_focal}'", "XMP:LensModel": f"'{lens_model}'",
+                                          "EXIF:Model": f"'{rewrite_model}'", "EXIF:ExifVersion": "0220"}
+                        # EXIF:FNumber
+                        copy_tags = ["EXIF:ApertureValue", "EXIF:FocalLengthIn35mmFormat", "EXIF:Flash",
+                                     "EXIF:LightSource", "EXIF:WhiteBalance", "EXIF:ISO", "EXIF:ShutterSpeedValue",
+                                     "EXIF:ExposureCompensation"
+                                     "EXIF:ExposureMode", "EXIF:MeteringMode", "EXIF:GPSLatitude", "EXIF:GPSAltitude",
+                                     "EXIF:GPSLatitudeRef", "EXIF:GPSSpeed", "EXIF:GPSAltitudeRef",
+                                     "EXIF:GPSProcessingMethod", "EXIF:GPSSpeedRef",
+                                     "EXIF:GPSLongitudeRef", "EXIF:GPSTimeStamp", "EXIF:GPSLongitude",
+                                     "EXIF:GPSDateStamp"]
+                        for tag in copy_tags:
+                            v = jpg_exif_info.get(tag)
+                            if v is not None:
+                                result_tag_map[tag] = v
+                            else:
+                                logging.warning(f"Tag {tag} can not be found in jpg file.")
+                        if "Composite:GPSPosition" not in jpg_exif_info:
+                            logging.warning(f"{dng_filename} Gps info not found.", )
+
+                        logging.debug(result_tag_map)
+
+                        items_ = ["-{}={}".format(k, v) for k, v in result_tag_map.items()]
+                        if DRY_RUN:
+                            logging.info(f"SIMULATE: {dng_full_path} {items_}")
+                        else:
+                            print(f"PROCESSING: {dng_full_path}")
+                            et.execute("-overwrite_original", "-preserve", "-FileCreateDate<CreateDate",
+                                       "-FileModifyDate<ModifyDate",
+                                       *items_,
+                                       f"-LensModel={lens_model}", f"-Model={rewrite_model}",
+                                       dng_full_path)
+                        print(f"Write {dng_full_path} Success! ")
+
+                        if DELETE_TRASH_JPG:
+                            # dng也要读一下，判断当前的jpg是不是raw
+                            dng_exif_info = et.get_tags(dng_full_path, "EXIF:ImageDescription")[0]
+                            is_trash_jpg = dng_exif_info.get("EXIF:ImageDescription") == "raw"
+                            if is_trash_jpg:
+                                if not DRY_RUN:
+                                    os.remove(jpg_full_path)
+                                logging.info(f"Trash jpg: {jpg_full_path} has been removed.")
+                    except ExifToolExecuteError as e:
+                        print(e.stderr)
+                        print(e.stdout)
+                        raise e
+                except KeyError:
+                    raise RuntimeError(
+                        f"Error: Unknown Focal:{exif_focal} in MODEL_CONFIG: {model_config["LensMap"]}")
+            except KeyError:
+                raise RuntimeError(f"Error: Unsupported Device: {exif_model}")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("""
+Usage:
+main.py <dng_files_path> [jpg_files_path] 
+        """)
+        exit(1)
+
+    DNG_DIR_PATH = sys.argv[1]
+    if len(sys.argv) == 2:
+        JPG_DIR_PATH = DNG_DIR_PATH
+    else:
+        JPG_DIR_PATH = sys.argv[2]
+
+    # do Logic
+    logging.info(f"""
+    DNG_Path: {DNG_DIR_PATH}
+    JPG_Path: {JPG_DIR_PATH}
+    """)
+    process()
